ce2c8c9dc30f620729f603e082f1eb7142f07cdd
lixinlu2000 <lixinlu2000@163.com>
2017-11-02 13:03:22 +0000

    accr_layers configure file
    accr_layers configure file

---
+++ algorithms/PARC/ACCR/accr_layers.m
@@ -0,0 +1,7 @@
+set_layers({'mac', 'neighborhood', 'eeabr', 'init_hello', 'app', 'stats'});
+set_layers({'mac', 'neighborhood', 'ant_routing', 'init_hello', 'app', 'stats'});
+set_layers({'mac', 'neighborhood', 'accr_original', 'init_hello', 'app', 'stats'});
+set_layers({'mac', 'neighborhood', 'check_duplicate', 'accr_mcbr', 'init_backward', 'app', 'stats'});
+set_layers({'mac', 'neighborhood', 'check_duplicate', 'accr_acs', 'init_backward', 'app', 'stats'});
+set_layers({'mac', 'neighborhood', 'check_duplicate', 'accr_mcbr', 'init_backward', 'app', 'stats'});
+set_layers({'mac', 'neighborhood', 'check_duplicate', 'mcbr_ant', 'init_backward', 'app', 'stats'});
